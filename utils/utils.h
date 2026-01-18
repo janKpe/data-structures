0ce6ef3b28b9b58d7365ff233dbd91b7bb76bc1e
Jan Kupke <jan.kupke@web.de>
2026-01-18 22:59:59 +0100

    util: make NULL-check macros variadic

--- utils/utils.h
+++ utils/utils.h
@@ -1,25 +1,35 @@
-#include "stdbool.h"
+#include <stdbool.h>
+
 #ifndef UTILS_H
 #define UTILS_H
 
-#define RETURN_NULL_IF_NULL(ptr)                                               \
+#define RETURN_NULL_IF_NULL(...)                                               \
   do {                                                                         \
-    if (ptr == NULL) {                                                         \
-      return NULL;                                                             \
+    const void* const _args[] = {__VA_ARGS__};                                 \
+    for (unsigned i = 0; i < sizeof(_args) / sizeof(_args[0]); i++) {          \
+      if (_args[i] == NULL) {                                                  \
+        return NULL;                                                           \
+      }                                                                        \
     }                                                                          \
   } while (0)
 
-#define RETURN_FALSE_IF_NULL(ptr)                                              \
+#define RETURN_FALSE_IF_NULL(...)                                              \
   do {                                                                         \
-    if (ptr == NULL) {                                                         \
-      return false;                                                            \
+    const void* const _args[] = {__VA_ARGS__};                                 \
+    for (unsigned i = 0; i < sizeof(_args) / sizeof(_args[0]); i++) {          \
+      if (_args[i] == NULL) {                                                  \
+        return false;                                                          \
+      }                                                                        \
     }                                                                          \
   } while (0)
 
-#define RETURN_IF_NULL(ptr)                                                    \
+#define RETURN_IF_NULL(...)                                                    \
   do {                                                                         \
-    if (ptr == NULL) {                                                         \
-      return;                                                                  \
+    const void* const _args[] = {__VA_ARGS__};                                 \
+    for (unsigned i = 0; i < sizeof(_args) / sizeof(_args[0]); i++) {          \
+      if (_args[i] == NULL) {                                                  \
+        return;                                                                \
+      }                                                                        \
     }                                                                          \
   } while (0)
 
